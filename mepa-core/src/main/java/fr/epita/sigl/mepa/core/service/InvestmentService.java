a729b90acdce305f979ba19d227b5ec09b324018
SuperKlyde <hugo.capes@epita.fr>
2016-07-20 17:08:41 +0200

    [INVEST][SERVICE] Created investment services

---
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/service/InvestmentService.java
@@ -0,0 +1,19 @@
+package fr.epita.sigl.mepa.core.service;
+
+import fr.epita.sigl.mepa.core.domain.Investment;
+
+import java.util.List;
+
+public interface InvestmentService {
+
+    void createInvestment(Investment investment);
+
+    void updateInvestment(Investment investment);
+
+    void deleteInvestment(Investment investment);
+
+    Investment getInvestmentById(Long id);
+
+    List<Investment> getAllInvestments();
+
+}
---
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/service/impl/InvestmentlServiceImpl.java
@@ -0,0 +1,47 @@
+package fr.epita.sigl.mepa.core.service.impl;
+
+import fr.epita.sigl.mepa.core.dao.InvestmentDao;
+import fr.epita.sigl.mepa.core.domain.Investment;
+import fr.epita.sigl.mepa.core.service.InvestmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Transactional
+public class InvestmentlServiceImpl implements InvestmentService {
+
+    @Autowired
+    private InvestmentDao investmentDao;
+
+    @Override
+    public void createInvestment(Investment investment) {
+        investment.setCreated(new Date());
+        this.investmentDao.create(investment);
+    }
+
+    @Override
+    public void updateInvestment(Investment investment) {
+        this.investmentDao.update(investment);
+    }
+
+    @Override
+    public void deleteInvestment(Investment investment) {
+        this.investmentDao.delete(investment);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Investment getInvestmentById(Long id) {
+        return this.investmentDao.getById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Investment> getAllInvestments() {
+        return this.investmentDao.getAll();
+    }
+}
