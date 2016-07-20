5b458b886d4d8149f162b2b9c6def29a1bd044c6
Lebel Gildas <lebel_g@epita.fr>
2016-07-21 00:48:59 +0200

    [PREINVEST][MODEL] Implement Project model with a display example

---
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/Project.java
@@ -0,0 +1,127 @@
+package fr.epita.sigl.mepa.core.domain;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Date;
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "Project.findById", query = "FROM Project p WHERE p.id=:id"),
+        @NamedQuery(name = "Project.findAll", query = "FROM Project p")
+        })
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    private Long user_id; //Id of the user creating the project
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "startDate", nullable = false)
+    private Date startDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "endDate", nullable = false)
+    private Date endDate;
+
+    @NotNull
+    private String name;
+
+    private String description;
+
+    private ArrayList<Image> images;
+
+
+
+    /*
+* ID
+* Name
+* UserID
+* Description
+* Date début
+* Date Fin
+* Photos
+* Vidéos
+* */
+    public Project() {
+        this.user_id = (long) 0;
+        this.endDate = new Date();
+        this.name = "Unnamed";
+    }
+
+    public Project(Long user_id, String projectName, Date endDate) {
+        this.user_id = user_id;
+        this.endDate = endDate;
+        this.name = projectName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<Image> getImages() {
+        return images;
+    }
+
+    public void setImages(ArrayList<Image> images) {
+        this.images = images;
+    }
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+}
--- mepa-front/src/main/webapp/WEB-INF/views/core/preinvest/projectDisplay.jsp
+++ mepa-front/src/main/webapp/WEB-INF/views/core/preinvest/projectDisplay.jsp
@@ -4,8 +4,9 @@
     <h2>Displaying a project</h2>
     <div class="alert alert-warning">
         Hello World ! :D
+
     </div>
     <div class="table-responsive">
-
+        Name: ${project.name}
     </div>
 </div>
