c9d1365dccd5b5cee028473627c68c90d0a5f61c
guillaume <guillaume.lasne@epita.fr>
2016-07-22 18:39:09 +0200

    Admin webpage + new feature click

--- mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/core/postinvest/AdminController.java
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/core/postinvest/AdminController.java
@@ -1,24 +1,14 @@
 package fr.epita.sigl.mepa.front.controller.core.postinvest;
 
-import fr.epita.sigl.mepa.core.domain.Model;
 import fr.epita.sigl.mepa.core.service.ModelService;
-import fr.epita.sigl.mepa.front.model.example.AddCustomModelFormBean;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/postinvest") // The adress of the component
@@ -27,14 +17,48 @@ public class AdminController {
 
     private static final Logger LOG = LoggerFactory.getLogger(AdminController.class);
 
+    private Integer clicks = 0;
+    private Integer totalAmount = 0;
 
     @Autowired
     private ModelService modelService;
 
-    @RequestMapping(value = {"/admin"}) // The adress to call the function
+    @RequestMapping(value = {"/admin"}) // The address to call the function
     public String showForm(HttpServletRequest request, ModelMap modelMap) {
         /* Code your logic here */
-
-        return "/postinvest/admin"; // The adress of the JSP coded in tiles.xml
+        request.setAttribute("clicks", clicks);
+        request.setAttribute("totalAmount", totalAmount);
+        return "/postinvest/admin"; // The address of the JSP coded in tiles.xml
     }
+
+    @RequestMapping(value = {"/admin/addAmount"}, method = {RequestMethod.POST})
+    public String addAmount(@RequestParam("amount") String amount)
+    //        ModelMap model, HttpSession session, HttpServletRequest request)
+    {
+        Integer adding = 0;
+        clicks++;
+        try {
+            adding = Integer.parseInt(amount);
+        }
+        catch (Exception e) {
+            String errorMessage = "Please enter a numerical number as donation amount.";
+            return "/invalid";
+        }
+        if(adding > 0) {
+            totalAmount += adding;
+        }
+        System.out.println(clicks);
+        System.out.println(amount);
+        System.out.println(totalAmount);
+        return "/invalid";
+    }
+/*
+    @RequestMapping(value = {"/projectCreate"}, method = RequestMethod.GET) // The adress to call the function
+    public String projectCreate(ModelMap modelMap) {
+        /* Code your logic here /
+        Project p = new Project();
+        modelMap.addAttribute(NEWPROJECT, p);
+        return "/preinvest/projectCreate"; // The adress of the JSP coded in tiles.xml
+    }
+*/
 }
--- mepa-front/src/main/webapp/WEB-INF/views/core/postinvest/admin.jsp
+++ mepa-front/src/main/webapp/WEB-INF/views/core/postinvest/admin.jsp
@@ -2,7 +2,24 @@
 
 <div class="container">
     <h2>Admin</h2>
-    <div class="alert alert-warning">
-        Hello Admin!
+
+    <div>
+        Le nombre de clicks total : <%= request.getAttribute("clicks") %>
+    </div>
+    <div>
+        La quantité donnée total : <%= request.getAttribute("totalAmount") %>
+    </div>
+
+    <div class="container">
+        <c:url var="addAmountUrl" value="/postinvest/admin/addAmount"/>
+        <form:form role="form" controller="AdminController" method="post"
+                   action="${addAmountUrl}">
+            <!--
+            <input id="amount" type="text" maxlength="20" placeholder="20" name="amount"/>
+            -->
+            <label for="amount">Quantité d'argent que vous voudriez donner</label>
+            <input id="amount" type="text" placeholder="20" name="amount"/>
+            <button type="submit" class="btn btn-default">Soumettre</button>
+        </form:form>
     </div>
 </div>
