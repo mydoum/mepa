2ec0e8aad0437cdbc2673c830d396687c8f75967
Simon MACE <simon.mace@epita.fr>
2016-07-22 03:25:13 +0200

    [FEATURE][INVESTCONTROLLER] now sending email when investing

--- mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/investment/InvestController.java
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/investment/InvestController.java
@@ -1,6 +1,7 @@
 package fr.epita.sigl.mepa.front.controller.investment;
 
 
+import com.sun.mail.smtp.SMTPTransport;
 import fr.epita.sigl.mepa.core.domain.Investment;
 import fr.epita.sigl.mepa.core.domain.User;
 import fr.epita.sigl.mepa.core.service.InvestmentService;
@@ -16,6 +17,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -26,6 +35,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Properties;
 
 
 @Controller
@@ -40,6 +50,10 @@ public class InvestController {
     @Autowired
     private ProjectService projectService;
 
+    static private Properties mailServerProperties;
+    static private Session getMailSession;
+    static private MimeMessage generateMailMessage;
+
     @RequestMapping(value = "/invest", method = RequestMethod.GET)
     public String invest(ModelMap model, HttpSession session) {
         float totalAmount = 0f;
@@ -51,18 +65,16 @@ public class InvestController {
 
 
     @RequestMapping(value = "/invest/investMoney", method = RequestMethod.POST)
-    public String investMoney(ModelMap model, HttpSession session, HttpServletRequest request) {
+    public String investMoney(ModelMap model, HttpSession session, HttpServletRequest request)  {
         float totalAmount = 0f;
 
         float moneyAmount = Float.parseFloat(request.getParameter("investAmount"));
         model.addAttribute("amount", moneyAmount);
-        Long userId = 2L;
+        Long userId = 1L;
         Long projectId = 1L;
 
         if (insertNewInvestor(moneyAmount, userId, projectId) == 0)
             return "/investment/investment";
-
-        //Mail.sendSMTPMail("smtp.gmail.com", true);
 
         ArrayList<Investor> listinvestors = getallinvestors(totalAmount);
         model.addAttribute("investorsList", listinvestors);
@@ -70,6 +82,7 @@ public class InvestController {
 
         return "/investment/investment";
     }
+
 
     private ArrayList<Investor> getallinvestors(float totalAmount) {
         ArrayList<Investment> investments = new ArrayList<Investment>(investmentService.getAllInvestments());
@@ -104,7 +117,37 @@ public class InvestController {
         newInvestment.setDate(date);
         investmentService.createInvestment(newInvestment);
 
+        try {
+            sendMail(userId, moneyAmount);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
         return 1;
+    }
+    private void sendMail (Long userId, float amountMoney) throws AddressException, MessagingException {
+        User tmpUser = userService.getUserById(userId);
+        String userMail = tmpUser.getLogin();
+        String userFirstName = tmpUser.getFirstName();
+        String userLastName = tmpUser.getLastName();
+
+        mailServerProperties = System.getProperties();
+        mailServerProperties.put("mail.smtp.port", "587");
+        mailServerProperties.put("mail.smtp.auth", "true");
+        mailServerProperties.put("mail.smtp.starttls.enable", "true");
+
+        getMailSession = Session.getDefaultInstance(mailServerProperties, null);
+        generateMailMessage = new MimeMessage(getMailSession);
+        generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(userMail));
+        generateMailMessage.setSubject("Greetings " + userFirstName + " " + userLastName);
+        String emailBody = "Thank you for donating. " + amountMoney + "<br><br> Regards, <br>MEPA Team";
+        generateMailMessage.setContent(emailBody, "text/html");
+
+        System.out.println("\n\n 3rd ===> Get Session and Send mail");
+        Transport transport = getMailSession.getTransport("smtp");
+
+        transport.connect("smtp.gmail.com", "mepa.epita@gmail.com", "sigl2017");
+        transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+        transport.close();
     }
 
     @RequestMapping(value = "/invest/filltables", method = RequestMethod.GET)
