e1839b6d25848a3317e8ab81c533e30667358bb7
Simon MACE <simon.mace@epita.fr>
2016-07-24 19:56:23 +0200

    [INVEST][MERGE] Resolving merge conflicts

--- mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/InsertDummyController.java
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/InsertDummyController.java
@@ -35,8 +35,6 @@ public class InsertDummyController {
 
     @Autowired
     private ProjectService projectService;
-    @Autowired
-    private RewardService rewardService;
 
     @Autowired
     private InvestmentService investmentService;
