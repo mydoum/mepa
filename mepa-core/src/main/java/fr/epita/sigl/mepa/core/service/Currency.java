7a9b92a385b0cbdb0798cf7be7c3eacb24d10cbd
Charles Claebots <claebo_c@epita.fr>
2016-07-28 13:34:30 +0200

    [PREINVEST][CURRENCY] corection symbol

--- mepa-core/src/main/java/fr/epita/sigl/mepa/core/service/Currency.java
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/service/Currency.java
@@ -4,9 +4,9 @@ package fr.epita.sigl.mepa.core.service;
  * Created by claebo_c on 28/07/16.
  */
 public enum Currency {
-    DOLLAR("D"),
-    EURO("E"),
-    POUND("P");
+    DOLLAR("$"),
+    EURO("€"),
+    POUND("£");
 
     public String getSymbol() {
         return symbol;
