513fb486eb82ca7bb5f12bbbcd3dee6d18b67209
prosper <prosperemmanu.mbadibayebec@epita.fr>
2016-07-28 23:08:50 +0200

    [SOCIAL] add off the Newsletter DAO for the like on the project

---
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/dao/NewsletterDAO.java
@@ -0,0 +1,18 @@
+package fr.epita.sigl.mepa.core.dao;
+
+import fr.epita.sigl.mepa.core.domain.CommentsModel;
+import fr.epita.sigl.mepa.core.domain.NewsletterModel;
+
+import java.util.List;
+
+/**
+ * Created by prosp_000 on 25/07/2016.
+ */
+public interface NewsletterDAO {
+    void create (NewsletterModel newslettermodel);
+    void update (NewsletterModel newslettermodel);
+    void delete (NewsletterModel newslettermodel);
+    NewsletterModel getById (Long Id);
+    List<NewsletterModel> getAll();
+}
+
