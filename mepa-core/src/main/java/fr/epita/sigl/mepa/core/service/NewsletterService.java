e29a2dae472529aea8aea5992ce2aef4800d0e28
prosper <prosperemmanu.mbadibayebec@epita.fr>
2016-07-28 23:11:39 +0200

    [SOCIAL] add off the NewsletterService for the like on the project

---
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/service/NewsletterService.java
@@ -0,0 +1,17 @@
+package fr.epita.sigl.mepa.core.service;
+
+import fr.epita.sigl.mepa.core.domain.CommentsModel;
+import fr.epita.sigl.mepa.core.domain.NewsletterModel;
+
+import java.util.List;
+
+/**
+ * Created by prosp_000 on 25/07/2016.
+ */
+public interface NewsletterService {
+    void createNewsletter(NewsletterModel newslettermodel);
+    void updateNewsletter(NewsletterModel newslettermodel);
+    void deleteNewsletter(NewsletterModel newslettermodel);
+    NewsletterModel getNewsletterById(Long id);
+    List<NewsletterModel> getAllNewsletterModels();
+}
