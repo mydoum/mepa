f85c152cb913cdf16c354b19945904e960cde485
Valentin ZHENG <zheng_v@epita.fr>
2016-07-29 05:39:03 +0200

    [FIX][PROJECT] Fixed infinite scroll for investor display. Functionnal now

--- mepa-front/src/main/webapp/resources/js/investment/infiniteScroll.js
+++ mepa-front/src/main/webapp/resources/js/investment/infiniteScroll.js
@@ -5,19 +5,17 @@ var initIndex = 0;
 var loadNumber = 25;
 
 $(document).ready(function () {
-    var win = $(window);
-    var doc = $(document);
-
     console.log("Inside Outside");
     // Each time the user scrolls
-    win.scroll(function () {
+    $(window).scroll(function () {
         // Vertical end reached?
-        if (doc.height() - win.height() == win.scrollTop() && loadNumber < infiniteScrollerElements.length) {
-            // New row
-            console.log("Inside Infinite Begin");
-            console.log("Init Index : " + initIndex);
-            console.log("Load Number : " + loadNumber);
+        console.log("Ratio window : " + ($(window).scrollTop() / $(window).height() * 100));
+        console.log("Ratio document: " + ($(window).scrollTop() / $(document).height()* 100));
 
+        if ($(window).scrollTop() / $(window).height() > 0.5 && $(window).scrollTop() / $(document).height() > 0.5
+            && loadNumber < infiniteScrollerElements.length) {
+            // New row
+            console.log("I am executed");
             for (var i = initIndex; i < loadNumber; ++i) {
                 var table = document.getElementById("infiniteInvestorsList");
                 var row = table.insertRow(i);
@@ -31,9 +29,6 @@ $(document).ready(function () {
             loadNumber =
                 loadNumber + 15 < infiniteScrollerElements.length ?
                 loadNumber + 15 : loadNumber + (infiniteScrollerElements.length - loadNumber);
-            console.log("Inside Infinite End");
-            console.log("Init Index : " + initIndex);
-            console.log("Load Number : " + loadNumber);
         }
     });
 });
